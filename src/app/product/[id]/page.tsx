f5f7661840953418da93f2f794901dcb163919f0
gravitymonk046-ux <gravitymonk046@gmail.com>
2026-04-12 01:17:57 +0530

    added more dats and fetures

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,7 +5,6 @@ import { notFound, useParams } from "next/navigation";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import CartDrawer from "@/components/layout/CartDrawer";
-import SearchOverlay from "@/components/layout/SearchOverlay";
 import ProductDetail from "@/components/product/ProductDetail";
 import { products } from "@/lib/products";
 
@@ -22,14 +21,12 @@ export default function ProductPage() {
     }
 
     const [cartOpen, setCartOpen] = useState(false);
-    const [searchOpen, setSearchOpen] = useState(false);
-    const [searchQuery, setSearchQuery] = useState("");
 
     const similarProducts = products.filter(p => p.id !== product.id).slice(0, 3);
 
     return (
         <div className="flex flex-col min-h-screen bg-background">
-            <Navbar onCartOpen={() => setCartOpen(true)} onSearchOpen={() => setSearchOpen(true)} />
+            <Navbar onCartOpen={() => setCartOpen(true)} />
 
             <main className="flex-1 mt-[80px]">
                 <ProductDetail product={product} similarProducts={similarProducts} />
@@ -37,7 +34,6 @@ export default function ProductPage() {
 
             <Footer />
             <CartDrawer open={cartOpen} onClose={() => setCartOpen(false)} />
-            <SearchOverlay open={searchOpen} onClose={() => setSearchOpen(false)} value={searchQuery} onChange={setSearchQuery} />
         </div>
     );
 }
